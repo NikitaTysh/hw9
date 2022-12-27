72da470c61428d300f5efe0d376e2c08ef2b54a5
NikitaTysh <nikita.tyshchenko7@gmail.com>
2022-12-27 23:31:33 +0100

    task 3 v1

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,30 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix';
+Notify.success('lib ok');
+console.log('ok');
+const btn = document.querySelector('.form button');
+const amount = document.querySelector('.form input[name="amount"]');
+const step = document.querySelector('.form input[name="step"]');
+const firstDelay = document.querySelector('.form input[name="delay"]');
+btn.addEventListener('click', onClick);
+function onClick(e) {
+  let count = +firstDelay.value;
+  e.preventDefault();
+  for (let i = 0; i < +amount.value; i += 1) {
+    createPromise(i + 1, count);
+    count += +step.value;
+    console.log(count);
   }
+}
+function createPromise(position, delay) {
+  const promise = new Promise((res, rej) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        res(Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`));
+      } else {
+        rej(Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`));
+      }
+    }, delay);
+  });
+  return promise;
 }
