978dd452829a0c01b490ea046bc306fc36e70856
NikitaTysh <99879360+NikitaTysh@users.noreply.github.com>
2023-01-16 20:32:19 +0000

    Deploying to gh-pages from @ NikitaTysh/hw9@1b53938b3b00693c04d9388688f3a752922cd9f5 🚀

--- 01-color-switcher.f9749ae5.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("body"),e=document.querySelector("button[data-start]"),a=document.querySelector("button[data-stop]");let r;function n(a){"data-start"===a.target.attributes[1].name?(r=setInterval((()=>{t.style.background=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),e.setAttribute("disabled","")):"data-stop"===a.target.attributes[1].name&&(e.removeAttribute("disabled"),clearInterval(r))}e.addEventListener("click",n),a.addEventListener("click",n);
-//# sourceMappingURL=01-color-switcher.f9749ae5.js.map
